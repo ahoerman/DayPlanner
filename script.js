f3dcad65e679d28fe19c994e15a51b78f4248c97
A Hoerman <awhoerman@me.com>
2020-02-03 00:30:41 -0500

    added time blocks

---
+++ script.js
@@ -0,0 +1,48 @@
+let m = moment().format(`MMMM Do YYYY`);
+
+$("#currentDay").text(m);
+
+ $("button.9").on("click", function(){
+     let newTask = $(".9").val();
+    console.log(newTask);
+ })
+
+ $("button.10").on("click", function(){
+    let newTask = $(".10").val();
+    console.log(newTask);
+})
+
+$("button.11").on("click", function(){
+    let newTask = $(".11").val();
+    console.log(newTask);
+})
+
+$("button.12").on("click", function(){
+    let newTask = $(".12").val();
+    console.log(newTask);
+})
+
+$("button.1").on("click", function(){
+    let newTask = $(".1").val();
+    console.log(newTask);
+})
+
+$("button.2").on("click", function(){
+    let newTask = $(".2").val();
+    console.log(newTask);
+})
+
+$("button.3").on("click", function(){
+    let newTask = $(".3").val();
+    console.log(newTask);
+})
+
+$("button.4").on("click", function(){
+    let newTask = $(".4").val();
+    console.log(newTask);
+})
+
+$("button.5").on("click", function(){
+    let newTask = $(".5").val();
+    console.log(newTask);
+})
